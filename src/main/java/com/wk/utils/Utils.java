15f2619cd228f6d6db64151706c3a571a8c12a6b
shishuai <shishuai2648@163.com>
2018-02-06 23:14:19 +0800

    文件夹监控完成

--- src/main/java/com/wk/utils/Utils.java
+++ src/main/java/com/wk/utils/Utils.java
@@ -4,6 +4,11 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+import static com.sun.deploy.cache.Cache.exists;
 
 /**
  * Created by lenovo on 2018/2/6.
@@ -18,17 +23,34 @@ public class Utils {
         return FileMove(oldFile, new File(file2));
     }
 
-    public static Boolean FileMove(File oldFile, File newFile) {
+    public static Boolean FileMove(final File oldFile, final File newFile) {
         if (!newFile.exists()) {
             newFile.mkdir();
         }
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String date = formatter.format(new Date());
+
+        String fileName = newFile.getAbsolutePath() + "//"+ date;
+
+        String suffix = oldFile.getName().substring(oldFile.getName().lastIndexOf("."));
+
+        File file = new File(fileName+suffix);
+        int i = 1;
+        while (file.exists()){
+            fileName = fileName+"("+i+")"+suffix;
+            file = new File(fileName);
+            i++;
+        }
         try {
-            FileUtils.copyFileToDirectory(oldFile,newFile);
-            FileUtils.deleteDirectory(oldFile);
+            FileUtils.copyFile(oldFile,file);
+            FileUtils.deleteQuietly(oldFile);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
+
+
 }
